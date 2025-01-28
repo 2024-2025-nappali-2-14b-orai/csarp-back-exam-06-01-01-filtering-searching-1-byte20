0fbecc08365355b536b7df780e15b2ebad9174f4
szt3bitger <szt3bitger@szt1-10-hp>
2025-01-28 09:14:04 +0100

    new propertik

--- Kreta.Shared/Models/Student.cs
+++ Kreta.Shared/Models/Student.cs
@@ -12,6 +12,8 @@ namespace Kreta.Shared.Models
         public SchoolClassType SchoolClass { get; set; }
         public string EducationLevel { get; set; } = string.Empty;
         public bool HasId => Id != Guid.Empty;
+        public bool IsWoman { get; set; } = false;
+        public bool IsMan => !IsWoman;
 
         public override string ToString()
         {
